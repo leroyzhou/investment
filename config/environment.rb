e7649760ae696e2734c09e256863226d1c3f86d0
leroyzhou <leroy.zhou@gmail.com>
2012-01-03 13:53:49 +0000

    optimize

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-
+puts File.dirname(__FILE__)
 # Initialize the rails application
 Investment::Application.initialize!
 
-require File.dirname(__FILE__)+'/../lib/lei/init'
+require File.expand_path('../lib/lei/init', __FILE__)#File.dirname(__FILE__)+'/../lib/lei/init'
 
 
 
